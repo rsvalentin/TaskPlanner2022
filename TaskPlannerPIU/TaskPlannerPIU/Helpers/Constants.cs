39a409da73350bd850d5fe743e94d84b76212ad3
monicabulboaca <monica.bulboaca.a@gmail.com>
2022-12-07 11:31:42 +0200

    groupBox pentru fiecare coloana

---
+++ TaskPlannerPIU/TaskPlannerPIU/Helpers/Constants.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPlannerPIU.Helpers
+{
+    public static class Constants
+    {
+        public static int COUNTER_LISTS = 1;
+
+        public static int ADD_LIST_COLUMN_POSITION_X = 20;
+        public static int ADD_LIST_COLUMN_POSITION_Y = 30;
+
+        public static int ADD_LIST_POSITION_X = 10;
+        public static int ADD_LIST_POSITION_Y = 45;
+
+        public static int QUIT_ADDING_LIST_POSITION_X = 62;
+        public static int QUIT_ADDING_LIST_POSITION_Y = 45;
+
+        public static int TITLE_LIST_POSITION_X = 12;
+        public static int TITLE_LIST_POSITION_Y = 25;
+
+        public static int MOVE_X = 110;
+        public static int MOVE_Y = 60;
+
+        public static int MOVE_GROUPBOX_X = 120;
+
+        public static int CARD_BTN_POSITION_X = 10;
+        public static int CARD_BTN_POSITION_Y = 45;
+
+        public static int LIST_GROUPBOX_POSITION_X = 5;
+        public static int LIST_GROUPBOX_POSITION_Y = 20;
+    }
+}
--- TaskPlannerPIU/TaskPlannerPIU/TaskPlannerWindow.cs
+++ TaskPlannerPIU/TaskPlannerPIU/TaskPlannerWindow.cs
@@ -1,126 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using static TaskPlannerPIU.Helpers.Constants;
 
 namespace TaskPlannerPIU
 {
     public partial class TaskPlannerWindow : Form
     {
+        private List<GroupBox> _groupBoxesLists = new List<GroupBox>();
         private MainWindow _parent;
         private TextBox _currentListTextBox;
-        private string _currentTitle;
-        private TextBox titleTextBox;
-        private Button addListButton;
-        private Button quitAddingListButton;
+        private string _selectedColumnName;
         private Button addCardButton;
-        private GroupBox listGroupBox;
-        private int _counterLists = 1;
-
-        private int addListPositionX = 5;
-        private int quitAddingListPositionX = 57;
-        private int titlePositionX = 5;
-        private int moveX = 10;
-        private int moveGroupBoxX = 120;
-        private int moveY = 60;
-        private int cardButtonPositionX = 10;
-        private int cardButtonPositionY = 45;
-        private int listGroupBoxPositionX = 5;
-        private int listGroupBoxPositionY = 20;
-
-        private List<GroupBox> groupBoxesLists = new List<GroupBox>();
+        private GroupBox _listGroupBox;
 
         public TaskPlannerWindow(MainWindow parent)
         {
             InitializeComponent();
             _parent = parent;
             this.AutoScroll = true;
+            this.saveListButton.Hide();
+            this.quitAddingListButton.Hide();
+            this.titleTextBox.Hide();
         }
 
         private void TaskPlannerWindow_Load(object sender, EventArgs e)
         {
             this.labelWelcome.Text += _parent.Username + "!";
-            groupBoxesLists.Add(this.initialListGroupBox);
+            _groupBoxesLists.Add(this.initialListGroupBox);
         }
 
         private void btnAddList_Click(object sender, EventArgs e)
         {
             this.btnAddList.Hide();
-            if (_counterLists != 1)
-            {
-                listGroupBox = new GroupBox();
-                groupBoxesLists.Add(listGroupBox);
-                listGroupBox.Location = new Point(listGroupBoxPositionX + moveGroupBoxX, listGroupBoxPositionY);
-                listGroupBox.Width = 150;
-                listGroupBox.Height = 230; // TODO: dynamic dupa nr de task uri din lista
-                listGroupBox.Controls.Add(btnAddList);
-            }
-            else
-                listGroupBox = initialListGroupBox;
+            this.saveListButton.Show();
+            this.quitAddingListButton.Show();
+            this.titleTextBox.Show();
 
-            titleTextBox = new TextBox();
-            titleTextBox.Location = _counterLists == 1 ? new Point(titlePositionX, 25) : new Point(titlePositionX, 25);
-            titlePositionX = titleTextBox.Location.X;
-            _currentTitle = titleTextBox.Text;
+            if (COUNTER_LISTS == 1)
+            {
+                _listGroupBox = initialListGroupBox;
+                this.groupBoxTasks.Controls.Add(_listGroupBox);
+            }
+            _listGroupBox.Controls.Add(titleTextBox);
+            _listGroupBox.Controls.Add(saveListButton);
+            _listGroupBox.Controls.Add(quitAddingListButton);
+            _selectedColumnName = titleTextBox.Text;
             titleTextBox.TextChanged += new System.EventHandler(this.titleTextBox_TextChanged);
             _currentListTextBox = titleTextBox;
-            listGroupBox.Controls.Add(titleTextBox);
-            labelWelcome.Location = new Point(3, 8);
-
-            addListButton = new Button();
-            addListButton.Location = _counterLists == 1 ? new Point(addListPositionX, 55) : new Point(addListPositionX, 55);
-            addListPositionX = addListButton.Location.X;
-            addListButton.Text = "Add list";
-            addListButton.Width = 50;
-            addListButton.Font = new Font("Microsoft Sans Serif", 7);
-            addListButton.Name = "btnAddAndSaveList";
-            addListButton.Click += new System.EventHandler(this.btnAddAndSaveList_Click);
-            listGroupBox.Controls.Add(addListButton);
-
-            quitAddingListButton = new Button();
-            quitAddingListButton.Location = _counterLists == 1 ? new Point(quitAddingListPositionX, 55) : new Point(quitAddingListPositionX, 55);
-            quitAddingListButton.Text = "Quit";
-            quitAddingListPositionX = quitAddingListButton.Location.X;
-            quitAddingListButton.Width = 50;
-            quitAddingListButton.Font = new Font("Microsoft Sans Serif", 7);
-            quitAddingListButton.Name = "btnQuitAddingList";
-            quitAddingListButton.Click += new System.EventHandler(this.quitAddingListButton_Click);
-            listGroupBox.Controls.Add(quitAddingListButton);
-
-            this.groupBoxTasks.Controls.Add(titleTextBox);
-            this.groupBoxTasks.Controls.Add(addListButton);
-            this.groupBoxTasks.Controls.Add(quitAddingListButton);
-            this.groupBoxTasks.Controls.Add(listGroupBox);
         }
 
-        private void btnAddAndSaveList_Click(object sender, EventArgs e)
-        {
-            _currentListTextBox.Text = _currentTitle;
-            // this.btnAddList.Location = new Point(titlePositionX, 25);
-            this.btnAddList.Show();
-            _counterLists++;
-
-            // add a list -> add card
-            addCardButton = new Button();
-            addCardButton.Location = new Point(cardButtonPositionX, cardButtonPositionY);
-            titleTextBox.Enabled = false;
-            addCardButton.Text = "Add card";
-            addListButton.Hide();
-            addListButton.Text = "Add list";
-            quitAddingListButton.Hide();
-            
-            listGroupBox.Controls.Add(addCardButton);
-        }
-
-        private void createNewListGroupBox()
-        {
-
-        }
 
         private void quitAddingListButton_Click(object sender, EventArgs e)
         {
@@ -129,25 +59,56 @@ namespace TaskPlannerPIU
 
         private void titleTextBox_TextChanged(object sender, EventArgs e)
         {
-            _currentTitle = titleTextBox.Text;
-        }
-
-        private void initialListGroupBox_Enter(object sender, EventArgs e)
-        {
-        }
-            private void groupBoxTasks_Enter(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TaskPlannerWindow_Paint(object sender, PaintEventArgs e)
-        {
-           // labelWelcome.Location = new Point(-this.AutoScrollPosition.X, this.Location.Y);
+            _selectedColumnName = titleTextBox.Text;
         }
 
         private void TaskPlannerWindow_Scroll(object sender, ScrollEventArgs e)
         {
-            labelWelcome.Location = new Point(3,8);
+            labelWelcome.Location = new Point(3, 8);
+        }
+
+        private void addListButton_Click(object sender, EventArgs e)
+        {
+            this.saveListButton.Hide();
+            this.quitAddingListButton.Hide();
+            this.titleTextBox.Hide();
+
+            COUNTER_LISTS++;
+            _currentListTextBox.Text = _selectedColumnName;
+            this.btnAddList.Show();
+
+            setColumnTitle();
+            createNewGroupBoxForColumn();
+        }
+
+        private void quitAddingListButton_Click_1(object sender, EventArgs e)
+        {
+            _currentListTextBox.Text = "";
+        }
+
+        private void setColumnTitle()
+        {
+            var textbox = new TextBox();
+            textbox.Location = this.titleTextBox.Location;
+            textbox.Width = this.titleTextBox.Width;
+            textbox.ReadOnly = true;
+            textbox.Font = new Font("Microsoft Sans Serif", 9, FontStyle.Bold);
+            textbox.BorderStyle = BorderStyle.FixedSingle;
+            textbox.Text = this.titleTextBox.Text;
+            this._listGroupBox.Controls.Add(textbox);
+            this.titleTextBox.Text = "";
+        }
+
+        private void createNewGroupBoxForColumn()
+        {
+            this._listGroupBox = new GroupBox();
+            this._groupBoxesLists.Add(_listGroupBox);
+            this._listGroupBox.Location = new Point(LIST_GROUPBOX_POSITION_X + MOVE_GROUPBOX_X, this.initialListGroupBox.Location.Y);
+            LIST_GROUPBOX_POSITION_X = _listGroupBox.Location.X;
+            this._listGroupBox.Width = initialListGroupBox.Width;
+            this._listGroupBox.Height = initialListGroupBox.Height; // TODO: dynamic dupa nr de task uri din lista
+            this._listGroupBox.Controls.Add(btnAddList);
+            this.groupBoxTasks.Controls.Add(_listGroupBox);
         }
     }
 }
